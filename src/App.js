e8e99b510bc507b98799e529ccf6e13864666f92
4bhishekKasam <konnurabhishek@gmail.com>
2019-02-24 10:36:54 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -19,6 +19,42 @@ class App extends Component {
     maxTemp: undefined
   };
 
+  getTodaysDate() {
+    return new Date();
+  }
+
+  getWeather = async e => {
+    e.preventDefault();
+    const city = e.target.elements.city.value.trim();
+    const country = e.target.elements.country.value.trim();
+
+    if (city && country) {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_KEY}&units=metric`;
+      const apiResponse = await fetch(url);
+      const data = await apiResponse.json();
+     // debugger;
+      if (data.cod === "404") {
+        this.setState({ error: "data not found" });
+      } else {
+        this.setState({
+          city: data.name,
+          country: data.sys.country,
+          temperature: data.main.temp,
+          error: undefined,
+          today: this.getTodaysDate(),
+          description: data.weather[0].description,
+          icon: data.weather[0].icon,
+          humidity: data.main.humidity,
+          minTemp: data.main.temp.min,
+          maxTemp: data.main.temp.max
+        });
+      }
+    } else {
+      this.setState({ error: "Enter Input Values" });
+    }
+    console.log(this.state);
+  };
+
   getGradient = temp => {
     if (temp < 0) return "linear-gradient(to right, #3E5151 ,#DECBA4)";
     else if (temp > 0 && temp < 20)
@@ -37,7 +73,7 @@ class App extends Component {
         style={{ backgroundImage: this.getGradient(this.state.temperature) }}
       >
         <Header />
-        <SearchForm />
+        <SearchForm getWeather={this.getWeather} />
       </div>
     );
   }
